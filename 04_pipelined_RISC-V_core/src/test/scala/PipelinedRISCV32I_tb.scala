313adf9af9153ca319db659420fc0a82d068dbdd
tojauch <jauch@eit.uni-kl.de>
2024-01-18 13:48:18 +0100

    Task 04 added

--- 04_pipelined_RISC-V_core/src/test/scala/PipelinedRISCV32I_tb.scala
+++ 04_pipelined_RISC-V_core/src/test/scala/PipelinedRISCV32I_tb.scala
@@ -1,20 +1,20 @@
 // ADS I Class Project
-// Multi-Cycle RISC-V Core
+// Pipelined RISC-V Core
 //
 // Chair of Electronic Design Automation, RPTU in Kaiserslautern
 // File created on 12/19/2023 by Tobias Jauch (@tojauch)
 
-package MultiCycleRV32I_Tester
+package PipelinedRV32I_Tester
 
 import chisel3._
 import chiseltest._
-import MultiCycleRV32I._
+import PipelinedRV32I._
 import org.scalatest.flatspec.AnyFlatSpec
 
-class MultiCycleRISCV32ITest extends AnyFlatSpec with ChiselScalatestTester {
+class PipelinedRISCV32ITest extends AnyFlatSpec with ChiselScalatestTester {
 
-"MultiCycleRV32I_Tester" should "work" in {
-    test(new MultiCycleRV32I("src/test/programs/BinaryFile")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+"PipelinedRV32I_Tester" should "work" in {
+    test(new PipelinedRV32I("src/test/programs/BinaryFile")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
 
       dut.clock.setTimeout(0)
       dut.clock.step(4)
