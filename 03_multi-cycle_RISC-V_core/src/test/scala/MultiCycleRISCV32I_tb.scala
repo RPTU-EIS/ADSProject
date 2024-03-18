0e2733b3bf2c1e46f9bb3ab232db4c29b99cc158
tojauch <jauch@eit.uni-kl.de>
2024-03-18 11:49:53 +0100

    added task 3

--- 03_multi-cycle_RISC-V_core/src/test/scala/MultiCycleRISCV32I_tb.scala
+++ 03_multi-cycle_RISC-V_core/src/test/scala/MultiCycleRISCV32I_tb.scala
@@ -16,47 +16,9 @@ class MultiCycleRISCV32ITest extends AnyFlatSpec with ChiselScalatestTester {
 "MultiCycleRV32I_Tester" should "work" in {
     test(new MultiCycleRV32I("src/test/programs/BinaryFile")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
 
-      dut.clock.setTimeout(0)
-      dut.clock.step(4)
-      dut.io.result.expect(0.U)     // ADDI x0, x0, 0
-      dut.clock.step(5)
-      dut.io.result.expect(4.U)     // ADDI x1, x0, 4
-      dut.clock.step(5)
-      dut.io.result.expect(5.U)     // ADDI x2, x0, 5
-      dut.clock.step(5)
-      dut.io.result.expect(9.U)     // ADD x3, x1, x2
-      dut.clock.step(5)
-      dut.io.result.expect(2047.U)  // ADDI x4, x0, 2047
-      dut.clock.step(5)
-      dut.io.result.expect(16.U)    // ADDI x5, x0, 16
-      dut.clock.step(5)
-      dut.io.result.expect(2031.U)  // SUB x6, x4, x5
-      dut.clock.step(5)
-      dut.io.result.expect(2022.U)  // XOR x7, x6, x3
-      dut.clock.step(5)
-      dut.io.result.expect(2047.U)  // OR x8, x6, x5
-      dut.clock.step(5)
-      dut.io.result.expect(0.U)     // AND x9, x6, x5
-      dut.clock.step(5)
-      dut.io.result.expect(64704.U) // SLL x10, x7, x2
-      dut.clock.step(5)
-      dut.io.result.expect(63.U)    // SRL x11, x7, x2
-      dut.clock.step(5)
-      dut.io.result.expect(63.U)    // SRA x12, x7, x2
-      dut.clock.step(5)
-      dut.io.result.expect(0.U)     // SLT x13, x4, x4
-      dut.clock.step(5)
-      dut.io.result.expect(0.U)     // SLT x13, x4, x5
-      dut.clock.step(5)
-      dut.io.result.expect(1.U)     // SLT x13, x5, x4
-      dut.clock.step(5)
-      dut.io.result.expect(0.U)     // SLTU x13, x4, x4
-      dut.clock.step(5)
-      dut.io.result.expect(0.U)     // SLTU x13, x4, x5
-      dut.clock.step(5)
-      dut.io.result.expect(1.U)     // SLTU x13, x5, x4
-      dut.clock.step(5)
-      dut.io.result.expect("hFFFFFFFF".U)
+        /* 
+         * TODO: Insert testcases from assignment 2 and adapt them for the multi-cycle core
+         */
            
     }
   }
