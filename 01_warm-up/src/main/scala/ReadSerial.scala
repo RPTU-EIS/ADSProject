b5649ecb19f15b8c2be2d58334cbb06015014199
tharinduSamare <tharindu.samare123@gmail.com>
2024-12-09 00:24:40 +0100

    added readSerial design. Testbench partially done

--- 01_warm-up/src/main/scala/ReadSerial.scala
+++ 01_warm-up/src/main/scala/ReadSerial.scala
@@ -8,37 +8,89 @@ package readserial
 
 import chisel3._
 import chisel3.util._
+import os.stat
 
 
 /** controller class */
-class Controller extends Module{
+class Controller(dataWidth: Int = 8) extends Module{
   
   val io = IO(new Bundle {
     /* 
      * TODO: Define IO ports of a the component as stated in the documentation
      */
+        // serial interface
+        val reset_n = Input(UInt(1.W))
+        val rxd     = Input(UInt(1.W))
+
+        // output interface
+        val valid   = Output(UInt(1.W))
+        
+        // internal IO
+        val cnt_s   = Input(UInt(4.W))
+        val cnt_en  = Output(UInt(1.W))
     })
 
   // internal variables
   /* 
    * TODO: Define internal variables (registers and/or wires), if needed
    */
+  val idleState :: runState :: Nil = Enum(2)
+
+  val state = RegInit(idleState)
+  val endOfPacket = RegNext(io.cnt_s === (dataWidth-1).U)
 
   // state machine
   /* 
    * TODO: Describe functionality if the controller as a state machine
    */
 
+    when (!io.reset_n) {
+        state := idleState // reset the FSM
+    }
+    .otherwise {
+        switch (state){
+            is(idleState){
+                when (io.rxd === 0.U) {
+                    state := runState
+                }
+            }
+            is (runState){
+                when (io.cnt_s === (dataWidth-1).U) {
+                    when (io.rxd === 1.U) {
+                        state := idleState
+                    }
+                    .otherwise{
+                        state := runState
+
+                    }
+                }
+            }
+            // is (doneState) {
+            //     when (io.rxd === 1.U) {
+            //         state := idleState
+            //     }
+            //     .elsewhen(io.rxd === 0.U) { // there is a consecutive next packet
+            //         state := runState
+            //     }
+            // }
+        }
+    }
+
+    io.cnt_en := state === runState // run the counter
+    io.valid := endOfPacket // data valid after all the bits are gathered.
+
 }
 
 
 /** counter class */
-class Counter extends Module{
+class Counter (dataWidth: Int = 8) extends Module{
   
   val io = IO(new Bundle {
     /* 
      * TODO: Define IO ports of a the component as stated in the documentation
      */
+        val cnt_s = Output(UInt(log2Ceil(dataWidth).W))
+        val cnt_en = Input(UInt(1.W))
     })
 
   // internal variables
@@ -50,28 +102,61 @@ class Counter extends Module{
   /* 
    * TODO: Describe functionality if the counter as a state machine
    */
+  val idleState :: runState :: Nil = Enum(2)
+  val state = RegInit(idleState)
 
+  val counter = RegInit(0.U(log2Ceil(dataWidth).W))
+
+  when(io.cnt_en === 0.U){
+    state := idleState
+  }
+  .otherwise{
+      switch (state) {
+        is (idleState){
+            counter := 0.U
+            when(io.cnt_en === 1.U){
+                state := runState
+            }
+        }
+        is (runState){
+            counter := counter + 1.U
+            when(counter === (dataWidth-1).U){
+                state := idleState
+            }
+        }
+      }
+  }
+
+  io.cnt_s := counter
 
 }
 
 /** shift register class */
-class ShiftRegister extends Module{
+class ShiftRegister(dataWidth: Int = 8) extends Module{
   
   val io = IO(new Bundle {
     /* 
      * TODO: Define IO ports of a the component as stated in the documentation
      */
+        val rxd = Input(UInt(1.W))
+        val data = Output(UInt(dataWidth.W))
     })
 
   // internal variables
   /* 
    * TODO: Define internal variables (registers and/or wires), if needed
    */
+  val shiftReg0 = RegInit(0.U(dataWidth.W))
+  val shiftReg1 = RegInit(0.U(dataWidth.W))
 
   // functionality
   /* 
    * TODO: Describe functionality if the shift register
    */
+  shiftReg0 := Cat(shiftReg0(dataWidth-2,0), io.rxd)
+  shiftReg1 := shiftReg0
+
+  io.data := shiftReg1
 }
 
 /** 
@@ -85,12 +170,16 @@ class ShiftRegister extends Module{
   * The valid signal goes high (‘1’) for one clock cycle after the last serial bit has been transmitted, 
   * indicating that a new data byte is ready.
   */
-class ReadSerial extends Module{
+class ReadSerial (dataWidth: Int = 8) extends Module{
   
   val io = IO(new Bundle {
     /* 
      * TODO: Define IO ports of a the component as stated in the documentation
      */
+        val reset_n = Input(UInt(1.W))
+        val rxd     = Input(UInt(1.W))
+        val valid   = Output(UInt(1.W))
+        val data    = Output(UInt(dataWidth.W))
     })
 
 
@@ -98,15 +187,27 @@ class ReadSerial extends Module{
   /* 
    * TODO: Instanciate the modules that you need
    */
+    val controllerInst  = Module(new Controller)
+    val counterInst     = Module(new Counter)
+    val shiftRegInst    = Module(new ShiftRegister)
 
   // connections between modules
   /* 
    * TODO: connect the signals between the modules
    */
 
+  counterInst.io.cnt_en        := controllerInst.io.cnt_en
+  controllerInst.io.cnt_s      := counterInst.io.cnt_s
+  
   // global I/O 
   /* 
-   * TODO: Describe output behaviour based on the input values and the internal signals
-   */
+  * TODO: Describe output behaviour based on the input values and the internal signals
+  */
+  controllerInst.io.reset_n    := io.reset_n
+  controllerInst.io.rxd        := io.rxd
+  shiftRegInst.io.rxd          := io.rxd
+
+  io.data   := shiftRegInst.io.data
+  io.valid  := controllerInst.io.valid
 
 }
--- 01_warm-up/src/test/scala/ReadSerialTester.scala
+++ 01_warm-up/src/test/scala/ReadSerialTester.scala
@@ -9,15 +9,16 @@ package readserial
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
-
+import scala.util.Random
 
 /** 
   *read serial tester
   */
 class ReadSerialTester extends AnyFlatSpec with ChiselScalatestTester {
 
+    val dataWidth = 8
   "ReadSerial" should "work" in {
-    test(new ReadSerial).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+    test(new ReadSerial(dataWidth = dataWidth)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
 
         /*dut.io.rxd.poke(...)
          *dut.clock.step(...)
@@ -26,6 +27,34 @@ class ReadSerialTester extends AnyFlatSpec with ChiselScalatestTester {
          *...
          *TODO: Add your testcases here
          */
+            val testRepeatCount = 5
+            // initialize dut
+            dut.io.rxd.poke(0.U)
+            dut.io.reset_n.poke(0.U)
+            dut.clock.step(1)
+            dut.io.reset_n.poke(1.U)
+
+            // test normal behavior
+            for ( _ <- 0 to testRepeatCount){
+
+                // wait before send a new packet (may be no wait)
+                dut.io.rxd.poke(1.U)
+                dut.clock.step(Random.nextInt(3)) //gap between 2 packets
+                // input a data packet
+                dut.io.rxd.poke(0.U)
+                dut.clock.step(1)
+
+                val dataVal = Random.nextInt(255) // generate a random data packet
+                println(s"dataVal : $dataVal")
+                for(bitIdx <- (dataWidth-1) to 0 by -1){
+                    dut.io.rxd.poke((dataVal.U(8.W))(bitIdx))
+                    // dut.io.valid.expect(0.U)   
+                    dut.clock.step(1)
+                }
+                // dut.io.valid.expect(1.U)
+                // dut.io.data.expect(dataVal.U)
+            }
+            dut.clock.step(5) // extra time to calculate output
         }
     } 
 }
