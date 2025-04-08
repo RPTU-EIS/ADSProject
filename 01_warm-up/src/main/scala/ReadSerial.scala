6a8294a63aa4408a31691a313e1cefa5334e9abd
Joel <zij22syh@rptu.de>
2025-04-08 11:03:06 +0200

    warm_up without test of readSerial (does not work yet)

--- 01_warm-up/src/main/scala/ReadSerial.scala
+++ 01_warm-up/src/main/scala/ReadSerial.scala
@@ -9,104 +9,128 @@ package readserial
 import chisel3._
 import chisel3.util._
 
-
 /** controller class */
+import chisel3._
+import chisel3.util._
+
+/** Controller class - manages the state of the serial receiver */
 class Controller extends Module{
-  
+
   val io = IO(new Bundle {
-    /* 
-     * TODO: Define IO ports of a the component as stated in the documentation
-     */
-    })
+    val reset_n = Input(Bool())    // Active-low reset
+    val rxd     = Input(Bool())    // Serial input
+    val cnt_s   = Input(Bool())    // Counter done signal (8 bits received)
+    val cnt_en  = Output(Bool())   // Enable signal for counter
+    val valid   = Output(Bool())   // High when byte reception is complete
+  })
 
-  // internal variables
-  /* 
-   * TODO: Define internal variables (registers and/or wires), if needed
-   */
+  val sIdle :: sReceiving :: Nil = Enum(2) //definition of posible state of the controller
+  val state = RegInit(sIdle)  // State start = sIdle
 
-  // state machine
-  /* 
-   * TODO: Describe functionality if the controller as a state machine
-   */
+  val validReg = RegInit(false.B)  // one-cycle valid pulse
 
+  io.cnt_en := false.B
+  io.valid  := validReg
+
+  validReg := false.B // default
+
+  when(!io.reset_n){      //when reset is active, then state is sIdle
+    state := sIdle
+  } .otherwise {          //if reset is unactive , then ..
+    switch (state) {
+      is(sIdle) {         //if state is sIdle
+        when(io.rxd === false.B) { //detect start bit
+          state := sReceiving
+        }
+      }
+
+      is(sReceiving) {    //if state is sReceiving
+        io.cnt_en := true.B         //enable counter
+        when(io.cnt_s) {            //if 8 bits received
+          validReg := true.B        //indicate valid byte
+          state := sIdle            //return to idle
+        }
+      }
+    }
+  }
 }
 
-
 /** counter class */
-class Counter extends Module{
-  
+class Counter extends Module {
   val io = IO(new Bundle {
-    /* 
-     * TODO: Define IO ports of a the component as stated in the documentation
-     */
-    })
+    val reset_n = Input(Bool())
+    val cnt_en  = Input(Bool())
+    val cnt_s   = Output(Bool())
+  })
 
-  // internal variables
-  /* 
-   * TODO: Define internal variables (registers and/or wires), if needed
-   */
+  val count = RegInit(0.U(3.W))
+  val done  = RegInit(false.B)
 
-  // state machine
-  /* 
-   * TODO: Describe functionality if the counter as a state machine
-   */
+  when(!io.reset_n) {
+    count := 0.U
+    done := false.B
+  } .elsewhen(io.cnt_en) {
+    when(count === 7.U) {
+      done := true.B
+      count := 0.U
+    } .otherwise {
+      done := false.B
+      count := count + 1.U
+    }
+  } .otherwise {
+    done := false.B
+  }
 
-
+  io.cnt_s := done
 }
 
 /** shift register class */
-class ShiftRegister extends Module{
-  
+class ShiftRegister extends Module {
   val io = IO(new Bundle {
-    /* 
-     * TODO: Define IO ports of a the component as stated in the documentation
-     */
-    })
+    val rxd  = Input(Bool())
+    val load = Input(Bool()) // enable shift
+    val data = Output(UInt(8.W))
+  })
 
-  // internal variables
-  /* 
-   * TODO: Define internal variables (registers and/or wires), if needed
-   */
+  val reg = RegInit(0.U(8.W))
 
-  // functionality
-  /* 
-   * TODO: Describe functionality if the shift register
-   */
+  when(io.load) {
+    // Correct MSB-first implementation:
+    // Shift existing bits right and insert new bit at MSB
+    reg := Cat(io.rxd, reg(7, 1))
+  }
+
+  io.data := reg
 }
 
-/** 
-  * The last warm-up task deals with a more complex component. Your goal is to design a serial receiver.
-  * It scans an input line (“serial bus”) named rxd for serial transmissions of data bytes. A transmission 
-  * begins with a start bit ‘0’ followed by 8 data bits. The most significant bit (MSB) is transmitted first. 
-  * There is no parity bit and no stop bit. After the last data bit has been transferred a new transmission 
-  * (beginning with a start bit, ‘0’) may immediately follow. If there is no new transmission the bus line 
-  * goes high (‘1’, this is considered the “idle” bus signal). In this case the receiver waits until the next 
-  * transmission begins. The outputs of the design are an 8-bit parallel data signal and a valid signal. 
-  * The valid signal goes high (‘1’) for one clock cycle after the last serial bit has been transmitted, 
-  * indicating that a new data byte is ready.
-  */
 class ReadSerial extends Module{
-  
+
   val io = IO(new Bundle {
-    /* 
-     * TODO: Define IO ports of a the component as stated in the documentation
-     */
-    })
+    val rxd     = Input(Bool())
+    val reset_n = Input(Bool())
+    val data    = Output(UInt(8.W))
+    val valid   = Output(Bool())
+  })
 
+  val controller = Module(new Controller)
+  val counter    = Module(new Counter)
+  val shifter    = Module(new ShiftRegister)
 
-  // instanciation of modules
-  /* 
-   * TODO: Instanciate the modules that you need
-   */
+  // Connect global inputs
+  controller.io.rxd     := io.rxd
+  controller.io.reset_n := io.reset_n
+  counter.io.reset_n    := io.reset_n
 
-  // connections between modules
-  /* 
-   * TODO: connect the signals between the modules
-   */
+  // Connect controller <-> counter
+  controller.io.cnt_s := counter.io.cnt_s
+  counter.io.cnt_en   := controller.io.cnt_en
 
-  // global I/O 
-  /* 
-   * TODO: Describe output behaviour based on the input values and the internal signals
-   */
+  // Connect controller -> shifter
+  shifter.io.rxd  := io.rxd
+  shifter.io.load := controller.io.cnt_en
 
+  // Outputs
+  io.data  := shifter.io.data
+  io.valid := controller.io.valid
 }
+
--- 01_warm-up/src/test/scala/ReadSerialTester.scala
+++ 01_warm-up/src/test/scala/ReadSerialTester.scala
@@ -14,19 +14,42 @@ import org.scalatest.flatspec.AnyFlatSpec
 /** 
   *read serial tester
   */
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
 class ReadSerialTester extends AnyFlatSpec with ChiselScalatestTester {
+  def sendBit(dut: ReadSerial, bit: Boolean): Unit = {
+    dut.io.rxd.poke(bit.B)
+    dut.clock.step(1)
+  }
 
-  "ReadSerial" should "work" in {
+  "ReadSerial" should "correctly receive one byte 0b10101010" in {
     test(new ReadSerial).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      // 1. Initialize
+      dut.io.reset_n.poke(true.B)
+      dut.io.rxd.poke(true.B) // Idle
+      dut.clock.step(2)
+      dut.io.valid.expect(false.B)
 
-        /*dut.io.rxd.poke(...)
-         *dut.clock.step(...)
-         *dut.io.valid.expect(...)
-         *dut.io.data.expect("b11111111".U) 
-         *...
-         *TODO: Add your testcases here
-         */
-        }
-    } 
+      // 2. Start bit
+      sendBit(dut, false) // Start bit (0)
+
+      // 3. Send data bits (MSB first)
+      val testByte = 0xAA.U
+      for (i <- 0 until 8) {
+        sendBit(dut, testByte(7 - i).asBool) // MSB first
+        dut.io.valid.expect(false.B)
+      }
+
+      // 4. Verify output
+      dut.clock.step(1) // Valid appears next cycle
+      dut.io.valid.expect(true.B)
+      dut.io.data.expect(0xAA.U) // Now correct!
+
+      // 5. Verify valid drops
+      dut.clock.step(1)
+      dut.io.valid.expect(false.B)
+    }
+  }
 }
-
