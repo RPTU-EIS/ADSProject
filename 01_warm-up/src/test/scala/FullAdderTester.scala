41d9effa755f18e85e23cbffa99f86a126a4c9a2
Quoc Hoang Le <tom77vil@rptu.de>
2025-11-26 22:41:45 +0700

    update Adder

--- 01_warm-up/src/test/scala/FullAdderTester.scala
+++ 01_warm-up/src/test/scala/FullAdderTester.scala
@@ -29,7 +29,71 @@ class FullAdderTester extends AnyFlatSpec with ChiselScalatestTester {
            *TODO: Insert your test cases
            */
 
+          // 000 00 a0 b0 ci0 s0 co0
+          dut.io.a.poke(0.U)
+          dut.io.b.poke(0.U)
+          dut.io.ci.poke(0.U)
+          dut.clock.step(1)
+          dut.io.s.expect(0.U)
+          dut.io.co.expect(0.U)
+
+          // 001 10 a0 b0 ci1 s1 co0
+          dut.io.a.poke(0.U)
+          dut.io.b.poke(0.U)
+          dut.io.ci.poke(1.U)
+          dut.clock.step(1)
+          dut.io.s.expect(1.U)
+          dut.io.co.expect(0.U)
+
+          // 010 10 a0 b1 ci0 s1 co0
+          dut.io.a.poke(0.U)
+          dut.io.b.poke(1.U)
+          dut.io.ci.poke(0.U)
+          dut.clock.step(1)
+          dut.io.s.expect(1.U)
+          dut.io.co.expect(0.U)
+
+          // 011 01 a0 b1 ci1 s0 co1
+          dut.io.a.poke(0.U)
+          dut.io.b.poke(1.U)
+          dut.io.ci.poke(1.U)
+          dut.clock.step(1)
+          dut.io.s.expect(0.U)
+          dut.io.co.expect(1.U)
+
+          // 100 10 a1 b0 ci0 s1 co0
+          dut.io.a.poke(1.U)
+          dut.io.b.poke(0.U)
+          dut.io.ci.poke(0.U)
+          dut.clock.step(1)
+          dut.io.s.expect(1.U)
+          dut.io.co.expect(0.U)
+
+          // 101 01 a1 b0 ci1 s0 co1
+          dut.io.a.poke(1.U)
+          dut.io.b.poke(0.U)
+          dut.io.ci.poke(1.U)
+          dut.clock.step(1)
+          dut.io.s.expect(0.U)
+          dut.io.co.expect(1.U)
+
+          // 110 01 a1 b1 ci0 s0 co1
+          dut.io.a.poke(1.U)
+          dut.io.b.poke(1.U)
+          dut.io.ci.poke(0.U)
+          dut.clock.step(1)
+          dut.io.s.expect(0.U)
+          dut.io.co.expect(1.U)
+
+          // 111 11 a1 b0 ci1 s1 co1
+          dut.io.a.poke(1.U)
+          dut.io.b.poke(1.U)
+          dut.io.ci.poke(1.U)
+          dut.clock.step(1)
+          dut.io.s.expect(1.U)
+          dut.io.co.expect(1.U)
+
         }
-    } 
+    }
 }
 
