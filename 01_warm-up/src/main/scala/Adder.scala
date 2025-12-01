8ce4e7382845744e75ef37886d24344f15fdd886
juriaan <deinnachbar25@gmail.com>
2025-12-01 13:45:51 +0100

    Full Adder

--- 01_warm-up/src/main/scala/Adder.scala
+++ 01_warm-up/src/main/scala/Adder.scala
@@ -10,78 +10,84 @@ import chisel3._
 import chisel3.util._
 
 
-/** 
-  * Half Adder Class 
-  * 
-  * Your task is to implement a basic half adder as presented in the lecture.
-  * Each signal should only be one bit wide (inputs and outputs).
-  * There should be no delay between input and output signals, we want to have
-  * a combinational behaviour of the component.
-  */
-class HalfAdder extends Module{
-  
+/**
+ * Half Adder Class
+ *
+ * Your task is to implement a basic half adder as presented in the lecture.
+ * Each signal should only be one bit wide (inputs and outputs).
+ * There should be no delay between input and output signals, we want to have
+ * a combinational behaviour of the component.
+ */
+class HalfAdder extends Module {
+
   val io = IO(new Bundle {
     val a = Input(Bool())
     val b = Input(Bool())
-    val s  = Output(Bool())
+    val s = Output(Bool())
     val c_o = Output(Bool())
-    })
+  })
 
   io.s := io.a ^ io.b
   io.c_o := io.a & io.b
 
 }
 
-/** 
-  * Full Adder Class 
-  * 
-  * Your task is to implement a basic full adder. The component's behaviour should 
-  * match the characteristics presented in the lecture. In addition, you are only allowed 
-  * to use two half adders (use the class that you already implemented) and basic logic 
-  * operators (AND, OR, ...).
-  * Each signal should only be one bit wide (inputs and outputs).
-  * There should be no delay between input and output signals, we want to have
-  * a combinational behaviour of the component.
-  */
-class FullAdder extends Module{
+/**
+ * Full Adder Class
+ *
+ * Your task is to implement a basic full adder. The component's behaviour should
+ * match the characteristics presented in the lecture. In addition, you are only allowed
+ * to use two half adders (use the class that you already implemented) and basic logic
+ * operators (AND, OR, ...).
+ * Each signal should only be one bit wide (inputs and outputs).
+ * There should be no delay between input and output signals, we want to have
+ * a combinational behaviour of the component.
+ */
+class FullAdder extends Module {
+
+  val s_t = Wire(Bool())
+  val c_o_t = Wire(Bool())
+  val c_o_t2 = Wire(Bool())
 
   val io = IO(new Bundle {
     val a = Input(Bool())
     val b = Input(Bool())
     val c_i = Input(Bool())
-    val s  = Output(Bool())
+    val s = Output(Bool())
     val c_o = Output(Bool())
-    })
+  })
 
+  val halfadder1 = Module(new HalfAdder)
+  val halfadder2 = Module(new HalfAdder)
 
-  /*
-   * TODO: Instanciate the two half adders you want to use based on your HalfAdder class
-   */
+  halfadder1.io.a := io.a
+  halfadder1.io.b := io.b
 
+  halfadder2.io.a := halfadder1.io.sum
+  halfadder2.io.b := io.c_i
 
-  /* 
-   * TODO: Describe output behaviour based on the input values and the internal signals
-   */
+  io.s := halfadder2.io.s
+  io.c_o := halfadder1.io.c_o | halfadder2.io.c_o
 
 }
 
-/** 
-  * 4-bit Adder class 
-  * 
-  * Your task is to implement a 4-bit ripple-carry-adder. The component's behaviour should 
-  * match the characteristics presented in the lecture.  Remember: An n-bit adder can be 
-  * build using one half adder and n-1 full adders.
-  * The inputs and the result should all be 4-bit wide, the carry-out only needs one bit.
-  * There should be no delay between input and output signals, we want to have
-  * a combinational behaviour of the component.
-  */
-class FourBitAdder extends Module{
+/**
+ * 4-bit Adder class
+ *
+ * Your task is to implement a 4-bit ripple-carry-adder. The component's behaviour should
+ * match the characteristics presented in the lecture.  Remember: An n-bit adder can be
+ * build using one half adder and n-1 full adders.
+ * The inputs and the result should all be 4-bit wide, the carry-out only needs one bit.
+ * There should be no delay between input and output signals, we want to have
+ * a combinational behaviour of the component.
+ */
+class FourBitAdder extends Module {
 
   val io = IO(new Bundle {
     /* 
      * TODO: Define IO ports of a 4-bit ripple-carry-adder as presented in the lecture
      */
-    })
+  })
 
   /* 
    * TODO: Instanciate the full adders and one half adderbased on the previously defined classes
