8dd1580e9460aafef9a2ca042bf31fdb6a0a7d02
Nithin J <nithinj613@gmail.com>
2026-06-13 22:33:19 +0000

    Implemented MEM barrier

--- 03_RV32I-pipeline/src/main/scala/MEMbarrier.scala
+++ 03_RV32I-pipeline/src/main/scala/MEMbarrier.scala
@@ -35,3 +35,27 @@ import chisel3._
 // -----------------------------------------
 
 //ToDo: Add your implementation according to the specification above here 
+class MEMBarrier extends Module {
+
+  val io = IO(new Bundle {
+    val inAluResult = Input(UInt(32.W))
+    val inRD = Input(UInt(5.W))
+    val inException = Input(Bool())
+
+    val outAluResult = Output(UInt(32.W))
+    val outRD = Output(UInt(5.W))
+    val outException = Output(Bool())
+  })
+
+  val aluResultReg = RegInit(0.U(32.W))
+  val rdReg = RegInit(0.U(5.W))
+  val exceptionReg = RegInit(false.B)
+
+  aluResultReg := io.inAluResult
+  rdReg := io.inRD
+  exceptionReg := io.inException
+
+  io.outAluResult := aluResultReg
+  io.outRD := rdReg
+  io.outException := exceptionReg
+}
